7568d7c091230d21cfbba24fefad9145a862aa0c
gnomek987 <klosep87@mail.ru>
2020-12-27 16:24:39 +0300

    Update 02  How old are you.py

--- 02  How old are you.py
+++ 02  How old are you.py
@@ -1,10 +1,14 @@
 print("Hellow")
 U_N = input ("what id your name  ?")
-print("Hellow",U_N)
+print("Hellow",U_N,"inyour name",len(U_N),'letters')
 U_Y = int(input ("when you was born?"))
-print(U_Y,"was a good year")
-a = 2020 - U_Y
-if a >= 100:
-    print ("you're",a,"years old. how are you still alive?")
+if len(str(U_N)) == 4:
+    print(U_Y,"was a good year")
+    a = 2020 - U_Y
+    if a >= 100:
+
+        print ("you're",a,"years old. how are you still alive?")
+    else:
+         print ("you're",a,"years old.")
 else:
-     print ("you're",a,"years old.")
+    print ("You make a little mistake")
