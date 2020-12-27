726d2477382b6ec0b6ec57ed063bf08792bf58d3
gnomek987 <klosep87@mail.ru>
2020-12-27 17:50:21 +0300

    Create 03 cinema.py

---
+++ 03 cinema.py
@@ -0,0 +1,7 @@
+c = input("what's your name")
+a = int(input("what year were you born"))
+if 2020 - a >= 18:
+    print(c,"do you want to watch an action movie?")
+else:
+    print(c,"want to watch a Comedy?")
+
