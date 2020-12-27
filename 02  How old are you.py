ed5b932c0eef437d135ff8ca5e24c34d3452cfcd
gnomek987 <klosep87@mail.ru>
2020-12-27 15:30:11 +0300

    Create 02  How old are you.py

---
+++ 02  How old are you.py
@@ -0,0 +1,10 @@
+print("Hellow")
+U_N = input ("what id your name  ?")
+print("Hellow",U_N)
+U_Y = int(input ("when you was born?"))
+print(U_Y,"was a good year")
+a = 2020 - U_Y
+if a >= 100:
+    print ("you're",a,"years old. how are you still alive?")
+else:
+     print ("you're",a,"years old.")
